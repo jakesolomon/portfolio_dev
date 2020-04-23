bcb4ba7006bcb84bcf9ef85ec1a43edcec734162
jakesolomon <jacob_solomon@student.uml.edu>
2020-04-23 08:58:18 -0400

    Email form is live
    Created email form from FormSpree. Adjusted onSubmit function to pull from state, and also find the form from form ID (rather than ev, which is undefined from my custom button). Addressed form styling issues. Added a safety net in custon button onClick,so it only fires if the prop exists.

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,11 +10,14 @@ class Contact extends Component {
     this.state = {
       name: "",
       email: "",
-      message: ""
+      message: "",
+      status: ""
+
     };
     this.handleChangeName = this.handleChangeName.bind(this);
     this.handleChangeEmail = this.handleChangeEmail.bind(this);
     this.handleChangeMessage = this.handleChangeMessage.bind(this);
+    this.submitForm = this.submitForm.bind(this);
   }
 
   handleChangeName(event) {
@@ -29,22 +32,56 @@ class Contact extends Component {
     this.setState( { message: event.target.value } );
   }
 
+  submitForm() {
+    this.setState({ status: "PENDING" });
+    const form = document.getElementById('contactForm');
+    var data = new FormData();
+    data.append('name', this.state.name);
+    data.append('email', this.state.email);
+    data.append('message', this.state.message);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) {console.log("xhr.readyState !== XMLHttpRequest.DONE"); return;}
+      if (xhr.status === 200) {
+        console.log(xhr.status);
+        this.setState({ name: "" });
+        this.setState({ email: "" });
+        this.setState({ message: "" });
+        this.setState({ status: "SUCCESS" });
+      } else {
+        console.log(xhr.status);
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+
   render() {
+
+    const { status } = this.state;
+
     return(
-      <form>
+      <form
+        id="contactForm"
+        action="https://formspree.io/mbjangoe"
+        method="POST"
+      >
         <label>
           Name:
-          <input type="text" value={this.state.name} onChange={this.handleChangeName} />
+          <input value={this.state.name} onChange={this.handleChangeName} />
         </label>
         <label>
           E-mail:
-          <input type="text" value={this.state.email} onChange={this.handleChangeEmail} />
+          <input type="email" name="email" value={this.state.email} onChange={this.handleChangeEmail} autocomplete="off" />
         </label>
         <label>
           Message:
-          <textarea value={this.state.message} onChange={this.handleChangeMessage} />
+          <textarea type="text" name="message" value={this.state.message} onChange={this.handleChangeMessage} autocomplete="off" />
         </label>
-        <Button text="Send Message" type="submit" value="Submit" />
+        {status === "SUCCESS" ? <p>Thank you!</p> : <Button clicked={this.state.status != ""} text="Send" onClick={() => this.submitForm()}/>}
+        {status === "ERROR" && <p>Oops! There was an error.</p>}
       </form>
     );
   }
